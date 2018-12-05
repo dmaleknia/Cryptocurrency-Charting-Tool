62db8b5fb0a7d6e0fa4cac770bdf33f06e7ab7f0
Dominic M <dominicccc@gmail.com>
2018-12-05 10:55:37 -0800

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, 'app-charting-tool/react-client/src');
+const DIST_DIR = path.join(__dirname, 'app-charting-tool/react-client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: "html-loader"
+        }]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
